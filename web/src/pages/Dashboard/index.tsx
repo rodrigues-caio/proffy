f65b294c799af63353278bc0b0a4da39cf680e56
Caio <caio.rodrigues.barbosa@usp.br>
2020-08-21 21:56:32 -0300

    creating dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import { useAuth } from '../../contexts/auth';
 
+import logoutIcon from '../../assets/images/icons/logout.svg';
+import avatarImage from '../../assets/images/avatarImage.jpeg';
+
+import './styles.css';
+
 function Dashboard() {
   const { signOut } = useAuth();
 
@@ -11,8 +16,26 @@ function Dashboard() {
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogout}>Logout</button>
+    <div id="page-dashboard">
+
+      <div id="page-dashboard-content" className="container">
+
+        <div className="content-top">
+          <div className="header">
+            <div className="avatar-name">
+              <img src={avatarImage} alt="Caio Rodrigues"/>
+              <span>Caio Rodrigues</span>
+            </div>
+
+            <button onClick={handleLogout}>
+              <img src={logoutIcon} alt="Sair"/>
+            </button>
+          </div>
+        </div>
+        
+      </div>
+    
+    </div>
     </>
   );
 };
