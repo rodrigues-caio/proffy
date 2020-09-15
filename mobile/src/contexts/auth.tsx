acc8ced7956e12e5a69f208e169a81605f411c18
Caio <caio.rodrigues.barbosa@usp.br>
2020-09-15 10:24:16 -0300

    adjust page

--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -1,15 +1,40 @@
-import React, { createContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
 
 import api from "../services/api";
 
-interface DataUserSession {
-  user: {
-    email: string;
-    password: string;
-  };
-  token: string;
+interface AuthProviderData {
+  signIn(data: object): Promise<void>;
 }
 
-const authContext = createContext<DataUserSession>({} as DataUserSession);
+const authContext = createContext<AuthProviderData>({} as AuthProviderData);
 
-export const AuthProvider: React.FC = () => {};
+export const AuthProvider: React.FC = () => {
+  const [user, setUser] = useState<object | null>(null);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = AsyncStorage.getItem("Proffy:user");
+      const storageToken = AsyncStorage.getItem("Proffy:token");
+
+      if (storageUser && storageToken) {
+        api.defaults.headers.Authorization = `Bearer ${storageToken}`;
+
+        setUser(JSON.parse(storageUser));
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
+  async function signIn(data: object) {
+    const response = await api.post("users/login", data);
+
+    setUser(response.data.user);
+
+    api.defaults.headers["Authorization"] = `Bearer ${response.data.token}`;
+
+    AsyncStorage.setItem("Proffy:user", JSON.stringify(response.data.user));
+    AsyncStorage.setItem("Proffy:token", response.data.token);
+  }
+};
