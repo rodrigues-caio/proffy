ce1f0799d51956a6aa031fb8d2ec1840b5d54752
Caio <caio.rodrigues.barbosa@usp.br>
2020-10-22 20:23:31 -0300

    feature: development page register

--- mobile/src/pages/Register/PartOne/index.tsx
+++ mobile/src/pages/Register/PartOne/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
 
+import backIncon from "../../../assets/images/icons/back.png";
+import styles from "./styles";
 
 const PartOne: React.FC = () => {
   return (
-    <View>
-      <Text>Part One</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <BorderlessButton>
+          <Image source={backIncon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <View style={styles.points}>
+          <View>
+            <Text>.</Text>
+          </View>
+          <View>
+            <Text>.</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ mobile/src/pages/Register/PartOne/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E5E5E5",
+    width: "100%",
+  },
+
+  header: {},
+
+  points: {},
+});
+
+export default styles;
