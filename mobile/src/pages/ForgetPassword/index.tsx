271167a1fbad620a8bffa2400058b96cfee38953
Caio <caio.rodrigues.barbosa@usp.br>
2020-10-22 19:56:14 -0300

    feature: creating page forget password

---
+++ mobile/src/pages/ForgetPassword/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ForgetPassword: React.FC = () => {
+  return (
+    <View>
+      <Text>Esqueceu a senha ?</Text>
+    </View>
+  );
+};
+
+export default ForgetPassword;
